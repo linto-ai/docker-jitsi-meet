fdfc30585987460cf660bf8bc8d023ed495c385c
Damien Laine <damien.laine@gmail.com>
2021-02-27 19:02:58 +0100

    First try with traefik in swarm mode

---
+++ examples/linto/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -uea
+. .env # Source all env
+rm -rf /home/linagora/linto_shared_mount/jitsi-meet-cfg
+mkdir -p /home/linagora/linto_shared_mount/jitsi-meet-cfg/{web/letsencrypt,transcripts,prosody/config,prosody/prosody-plugins-custom,jicofo,jvb,jigasi,jibri}
+CONFIG=/home/linagora/linto_shared_mount/jitsi-meet-cfg \
+MY_DOMAIN="jitsi.linto.ai" \
+
+docker stack deploy --compose-file linto.yml linto_jitsi
